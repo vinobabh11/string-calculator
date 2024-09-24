a1c04e87a6d159215098f3df7e303d95c2d5a548
vinoba <vinoba.bh.11@gmail.com>
2024-09-24 22:38:31 +0530

    testing step 4th Support different delimiters:

--- src/utils/Calculator.test.js
+++ src/utils/Calculator.test.js
@@ -15,3 +15,7 @@ test('empty string returns 0', () => {
   test('handle new lines between numbers', () => {
     expect(add("1\n2,3")).toBe(6);
   });
+
+  test('custom delimiter', () => {
+    expect(add("//;\n1;2")).toBe(3);
+  });

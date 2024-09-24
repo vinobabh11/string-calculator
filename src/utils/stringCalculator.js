d49c579ee3c86d1c9d172b5352078b5ccc84df6b
vinoba <vinoba.bh.11@gmail.com>
2024-09-24 22:22:55 +0530

    Implementation of step 2 Allow the add method to handle any amount of numbers.

--- src/utils/stringCalculator.js
+++ src/utils/stringCalculator.js
@@ -1,8 +1,20 @@
 export function add(numbers) {
     if (!numbers) return 0;
     let delimiter = /,|\n/;
-    
+
+    if (numbers.startsWith('//')) {
+        const parts = numbers.split('\n', 2);
+        delimiter = new RegExp(parts[0].slice(2));
+        numbers = parts[1];
+    }
+
     const numArray = numbers.split(delimiter).map(Number);
+
+    const negatives = numArray.filter(num => num < 0);
+    if (negatives.length) {
+        throw new Error(`Negative numbers not allowed: ${negatives.join(', ')}`);
+    }
+
     return numArray.reduce((sum, num) => sum + (isNaN(num) ? 0 : num), 0);
 
 }
